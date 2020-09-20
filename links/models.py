dd851a7e20e06e8766540b0379d25273467d5fb0
Farhad Hossain <farhadhossain0085@gmail.com>
2020-09-21 01:27:05 +0600

    model updated for adding lazy

--- links/models.py
+++ links/models.py
@@ -12,5 +12,10 @@ class LinkDetail(models.Model):
     link = models.OneToOneField(Link, on_delete=models.CASCADE)
     section = models.TextField()
 
+    # if a link doesn't change after 50 check, lazy will be true
+    # next time it won't check for changes
+    # will check again after 2 hours
+    lazy = models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.link.id} -  {self.link}"
