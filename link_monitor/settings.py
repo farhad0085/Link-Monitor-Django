4680181c5e0307d4506063ac75306c9c15cf2b83
Farhad Hossain <farhadhossain0085@gmail.com>
2020-10-12 02:44:02 +0600

    db

--- link_monitor/settings.py
+++ link_monitor/settings.py
@@ -66,16 +66,24 @@ WSGI_APPLICATION = 'link_monitor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DBNAME'),
+#         'USER': os.getenv('DBUSER'),
+#         'PASSWORD': os.getenv('DBPASS'),
+#         'HOST': os.getenv('DBHOST'),
+#         'PORT': os.getenv('DBPORT')
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DBNAME'),
-        'USER': os.getenv('DBUSER'),
-        'PASSWORD': os.getenv('DBPASS'),
-        'HOST': os.getenv('DBHOST'),
-        'PORT': os.getenv('DBPORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
 
